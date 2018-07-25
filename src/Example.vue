7b4d91c02d10f2231b082e9274ef5aa80b99e8a2
Bartosz Markowski <bartosz.markowski@mindseater.com>
2018-07-25 17:06:21 +0200

    working custom component

---
+++ src/Example.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <button v-on:click="increase">click</button>
+    <div>{{ i }}</div>
+  </div>
+</template>
+<script>
+  export default {
+    props: ['when'], 
+    data: function () {
+      return {
+        i: 0
+      }
+    },
+    methods: {
+      increase: function () {
+        this.i++;
+        if(this.i == this.when) {
+           this.$emit('equal', this.i);
+        }
+      }
+    }
+  }
+</script>
